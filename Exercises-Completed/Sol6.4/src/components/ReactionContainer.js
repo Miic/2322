0e29a4dff53d8176630e4508f935231b40e55d2c
Pat Haggerty <hagg@haggconsulting.com>
2018-10-14 20:27:38 -0400

    -6.4

--- Exercises-Completed/Sol6.4/src/components/ReactionContainer.js
+++ Exercises-Completed/Sol6.4/src/components/ReactionContainer.js
@@ -2,18 +2,31 @@ import React from 'react'
 import Reaction from './Reaction'
 
 export default class ReactionContainer extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            reactionList:[
+                {id: 0, question: "Which dressing is tastier?", 
+                    answer1: "Ranch", answer2: "Vinaigrette"},
+                {id: 1, question: "Which has the better mane?", 
+                    answer1: "Lion", answer2: "Horse", imageUrl:"/assets/lion.png"},
+                {id: 2, question: "Which is faster?", 
+                    answer1: "Cheetah", answer2: "Car", imageUrl:"/assets/cheetah.png"},
+                {id: 3, question: "Which bird has the heavier legs?", 
+                    answer1: "Turkey", answer2: "Ostrich", imageUrl:"/assets/ostrich.png"} ]
+        };
+    }
+
     render(){
 
         return(
         <div className="container">
             <div className="row">
-                <Reaction question="Which bird is from the Struthionidae family?" answer1="Ostrich" 
-                    answer2="Blue jay"  imageUrl="/assets/ostrich.png" reactionSeconds={20000} />
-                <Reaction />
-                <Reaction question="Which is the better Apex predator?" answer1="Lion" 
-                    answer2="Human" imageUrl="/assets/lion.png" />
-                <Reaction question="Which is faster?" answer1="Cheetah" reactionSeconds={20000} 
-                    answer2="Ostrich" imageUrl="/assets/cheetah.png" />
+            {
+                this.state.reactionList.map(aReaction=>
+                    <Reaction {...aReaction} key={aReaction.id}/>)
+            }
             </div>
         </div>
         );
--- Exercises-Completed/Sol7.1/src/App.js
+++ Exercises-Completed/Sol7.1/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
 import UIRoot from './components/UIRoot';
 
-export default class App extends Component {
+class App extends Component {
+  
+  render() {
+    return (
+      <UIRoot />
+    );
+  }
+}
 
-    render() {
-        return <UIRoot />;
-    }
-
-};
+export default App;
